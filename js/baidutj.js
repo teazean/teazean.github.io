c31320ed55551804b09df37304bea5845e2e3410
teazean <zhanglei55@zhanglei-mac.local>
2015-10-28 14:46:32 +0800

    baidutj

---
+++ js/baidutj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?70ec9118c8b324ea1b3aec03ab8166dd";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
